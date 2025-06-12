adb2d61e0b90a226abaaa6f3d9b544ad212ac3da
miyn-isi <ishii.ma@kanamekey.com>
2025-06-12 10:50:54 +0900

    fix accsess token

---
+++ lib/google-auth.ts
@@ -0,0 +1,11 @@
+import { GoogleAuth } from 'google-auth-library';
+
+const auth = new GoogleAuth({
+    scopes: ['https://www.googleapis.com/auth/cloud-platform'],
+});
+
+export async function getAccessToken(): Promise<string | undefined> {
+    const client = await auth.getClient();
+    const tokenResponse = await client.getAccessToken();
+    return tokenResponse?.token ?? undefined;
+}
